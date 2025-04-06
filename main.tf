de2551821fab4aba2ecf67629eef2288adfb1b58
Ramya Ranjan Sahoo <114637302+Theramya10@users.noreply.github.com>
2025-04-06 11:33:33 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "eks_vpc" {
 # Subnet-1
 resource "aws_subnet" "eks_public_1" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
